d1fcfa62e6a717855f3969e2814f86aadee111cb
nianqin <nianqin.vip@gmail.com>
2022-07-24 17:46:17 +0800

    添加zh-check提取中文

---
+++ bin/zh-check/index.js
@@ -0,0 +1,309 @@
+#!/usr/bin/env node
+
+"use strict";
+const program = require("commander");
+const utils = require("../utils");
+const { loadConfig } = require("../configuration");
+const vfs = require("vinyl-fs");
+const map = require("map-stream");
+const path = require("path");
+const fs = require("fs");
+
+function commaSeparatedList(value, split = ",") {
+  return value.split(split).filter(item => item);
+}
+
+program
+  .version(require('../../package.json').version)
+  .option("--cwd <path>", "工作目录")
+  .option("--root-dir <path>", "国际文本所在的根目录")
+  .option(
+    "--config <path>",
+    "配置文件的路径，没有配置，默认路径是在${cwd}/vve-i18n-cli.config.js"
+  )
+  .option("--no-config", "是否取配置文件")
+  .option(
+    "--i18n-file-rules <items>",
+    "匹配含有国际化文本的文件规则",
+    commaSeparatedList
+  )
+  .option(
+    "--ignore-i18n-file-rules <items>",
+    "不匹配含有国际化文本的文件规则",
+    commaSeparatedList
+  )
+  .option(
+    "--ignore-pre-geg <items>",
+    "被忽略的前缀，是个数组",
+    commaSeparatedList
+  )
+  .option("--i18n-import-for-js <item>", "js相关文件需要引入的国际化文件")
+  .option("--js-i18n-func-name <item>", "js相关文件需要使用国际化方法")
+  .option("--vue-i18n-func-name <item>", "vue相关文件需要使用的国际化方法")
+  .parse(process.argv);
+
+const config = {
+  // 工作目录
+  cwd: ".",
+  // 根目录，国际文本所在的根目录
+  rootDir: "src",
+  // 配置文件的路径，没有配置，默认路径是在${cwd}/vve-i18n-cli.config.js
+  config: undefined,
+  // 是否取配置文件
+  noConfig: false,
+  // 匹配含有国际化文本的文件规则
+  i18nFileRules: ["**/*.+(vue)"],
+  // 不匹配含有国际化文本的文件规则
+  ignoreI18nFileRules: [],
+  // 被忽略的前缀
+  ignorePreReg: [
+    new RegExp("//.+"),
+  ],
+  // 国际化文本的正则表达式，正则中第一个捕获对象当做国际化文本
+  i18nTextRules: [
+    /(?:[\$.])t\(['"](.+?)['"]/g
+  ],
+};
+
+Object.assign(config, program);
+
+const CONFIG_JS_FILENAME = "vve-i18n-cli.config.js";
+
+let absoluteCwd = path.resolve(config.cwd);
+
+// 优先判断是否需要读取文件
+if (!config.noConfig) {
+  let configFilePath = path.join(absoluteCwd, CONFIG_JS_FILENAME);
+  if (config.config) {
+    configFilePath = path.resolve(config.config);
+  }
+  if (fs.existsSync(configFilePath)) {
+    const conf = loadConfig(configFilePath);
+    if (conf && conf.options && conf.options.zhCheck) {
+      Object.assign(config, conf.options.zhCheck, program);
+    }
+  }
+}
+
+// 制定配置文件后，cwd在配置文件中定义，则cwd就需要重新获取
+if (!program.cwd) {
+  absoluteCwd = path.resolve(config.cwd);
+}
+
+const { ignorePreReg, i18nImportForJs, jsI18nFuncName, vueI18nFuncName } = config
+
+const absoluteRootDir = path.resolve(absoluteCwd, config.rootDir);
+
+// 匹配中文字符的正则表达式： [\u4e00-\u9fa5] // https://www.w3cschool.cn/regexp/nck51pqj.html
+// 匹配双字节字符(包括汉字在内)：[^\x00-\xff] // https://www.w3cschool.cn/regexp/nck51pqj.html
+// (?!\$t\()([^\x00-\xff]+) 不已啥开头
+// ([^\x00-\xff]+)
+// 匹配中文
+const regI18n = new RegExp(/([^\x00-\xff]+)/, "g");
+
+// 左边是否是>
+function letfRt (str, startIndex, range = 50) {
+  const end = startIndex - range
+  for (let i = startIndex; i >= end; i--) {
+    if (str.charAt(i) === '>') return true
+    if (!str.charAt(i).trim()) continue
+    return false
+  }
+  return false
+}
+// 右边是否是<
+function rightLt (str, startIndex, range = 50) {
+  const end = startIndex + range
+  for (let i = startIndex; i <= end; i++) {
+    if (str.charAt(i) === '<') return true
+    if (!str.charAt(i).trim()) continue
+    return false
+  }
+  return false
+}
+// 是否在 > 之间 <
+function betweenRtAndLt (strContent, match, index, range) {
+  return letfRt(strContent, index - 1, range) && rightLt(strContent, match.length + index, range)
+}
+
+// 获取当前元素所在行之前的元素
+function getLinePreText(str, match, index, range = 300) {
+  const startIndex = index - 1
+  let end = startIndex - range
+  for (let i = startIndex; i >= end; i--) {
+    if (str.charAt(i) === '\n') {
+      end = i
+      break;
+    }
+  }
+  return str.slice(end, index)
+}
+
+// 获取当前元素所在行之后的元素
+function getLineSubfixText(str, match, index, range = 300) {
+  const startIndex = match.length + index
+  let end = startIndex + range
+  for (let i = startIndex; i <= end; i++) {
+    if (str.charAt(i) === '\n') {
+      end = i
+      break;
+    }
+  }
+  return str.slice(startIndex, end)
+}
+
+// 判定是否被双斜杆注释包裹
+function isWrapByDoubelSlashComment (str, match, index, range = 500) {
+  const linePreText = getLinePreText(str, match ,index, range)
+  return linePreText.indexOf('//') !== -1
+}
+
+const i18nWrapPrefixReg = /t\s*\(\s*$/
+// 是否被$t包裹 $t("你好") 识别出来的中文
+function isWrapByI18n (str, match, index, range) {
+  // const subfixText = getLineSubfixText(str, match, index, range) // 不判断后缀了，直接判定前缀
+  // if (subfixText.trim().charAt(0) !== ')') return false
+  const linePreText = getLinePreText(str, match ,index, range)
+  if (!i18nWrapPrefixReg.test(linePreText.trim())) return false
+  return true
+}
+
+// 前缀是否满足要求
+function prefixTestReg (reg, str, match, index, range) {
+  const linePreText = getLinePreText(str, match ,index, range)
+  return new RegExp(reg).test(linePreText.trim())
+}
+
+// 国际化文本，中文开头，可以包含中文数字.和空格，用户匹配
+const i18nContentReg = /(?![{}A-Za-z0-9.©×\-_!, ]+)([^\x00-\xff]|[A-Za-z0-9.©×\-_!, ])+/g
+// 判定是否包含中文，用于test
+const i18nContenTestReg = /^(?![A-Za-z0-9.©×\-_!, ]+$)([^\x00-\xff]|[A-Za-z0-9.©×\-_!, ])+$/
+// 处理template
+const templateReg = new RegExp("<template>([\\s\\S]+)<\\/template>", "i")
+// 处理script
+const scriptReg = new RegExp("<script>([\\s\\S]+)<\\/script>", "i")
+// tag的内容正则匹配
+const TagContentReg = new RegExp('>((?:[^\x00-\xff]|\w|[0-9{}.A-Za-z\\s])+)<', 'g')
+// html start tag匹配正则
+const startTagReg = new RegExp(/<(?:[-A-Za-z0-9_]+)((?:\s+[a-zA-Z_:@][-a-zA-Z0-9_:.]*(?:\s*=\s*(?:(?:"[^"]*")|(?:'[^']*')|[^>\s]+))?)*)\s*(?:\/?)>/, 'g')
+// 属性的正则
+const attrReg = /([@:a-zA-Z_][-a-zA-Z0-9_.]*)(?:\s*=\s*(?:(?:"((?:\\.|[^"'])*)")|(?:'((?:\\.|[^'"])*)')))/g;
+// 前后非空白，这里必须是三个字符
+const nonPreSubWhiteReg = /\S.+\S/
+// 国际化字符串，被单引号或者双引号包裹，内容中文开头
+const i18nStrReg = /"((?![{}A-Za-z0-9.©×\-_!, ]+)(?:[^\x00-\xff]|[A-Za-z0-9.©×\-_!, ])+)"|'((?![{}A-Za-z0-9.©×\-_!, ]+)(?:[^\x00-\xff]|[A-Za-z0-9.©×\-_!, ])+)'/g
+
+// 解析vue文件
+function processVueFile2 (fileContent) {
+  // 过滤出template相关内容，处理tag内容的国际化
+  let newFileContent = fileContent.replace(templateReg, function (match, templateKey, index) {
+    // 经过TagContentReg的过滤，$()"",这些关键字都不会被当作国际化文本处理
+    let newTemplateKey = templateKey.replace(TagContentReg, function (match, tagContentKey, index) {
+      if (!tagContentKey.trim()) return match
+      // console.log(match, tagContentKey)
+      // 经过这一层过滤，会过滤去tag内容的中文，并加上国际化文本
+      const newTagContentKey = tagContentKey.replace(i18nContentReg, function (match) {
+        const trimMatch = match.trim()
+        // 例子 <p> 啦啦啦 </p>  变成 <p> {{$t('啦啦啦')}} </p>
+        return match.replace(trimMatch, `{{${vueI18nFuncName}('${trimMatch}')}}`)
+      })
+      return match.replace(tagContentKey, newTagContentKey)
+    })
+    return match.replace(templateKey, newTemplateKey)
+  })
+  // console.log(newFileContent)
+  // 过滤出template相关内容，处理tag属性的国际化
+  newFileContent = newFileContent.replace(templateReg, function (match, templateKey, index) {
+    // 过滤出属性 <p title="希望的田野">
+    let newTemplateKey = templateKey.replace(startTagReg, function (match, key, index) {
+      const attrStr = key
+      if (!attrStr.trim()) return match
+      const newAttStr = attrStr.replace(attrReg, function (match, name, doubleQuoteValue, singleQuoteValue) {
+        const value = doubleQuoteValue || singleQuoteValue
+        if (name.charAt(0) === '@' || name.charAt(0) === ':') return match
+        if (!i18nContenTestReg.test(value)) return match
+        // console.log(arguments)
+        // vueI18nFuncName = '$t' => `$t(${value})`
+        return `:${name}="${vueI18nFuncName}('${value}')"`
+      })
+      return match.replace(attrStr, newAttStr)
+    })
+    return match.replace(templateKey, newTemplateKey)
+  })
+  // console.log(newFileContent)
+  // 过滤出script相关内容，过滤出被引号包裹的中文字符串，对这种类型进行替换国际化替换
+  newFileContent = newFileContent.replace(scriptReg, function (match, scriptKey, index) {
+    let newScriptKey = scriptKey.replace(i18nStrReg, function (match, key, key2, index) {
+      for (let i = 0; i < ignorePreReg.length; i++) {
+        if (prefixTestReg(ignorePreReg[i], scriptKey, match, index, 50)) return match
+      }
+      // vueI18nFuncName = '$t' => `this.$t(${match})`
+      return `this.${vueI18nFuncName}(${match})`
+    })
+    return match.replace(scriptKey, newScriptKey)
+  })
+  // console.log(newFileContent)
+  return newFileContent
+}
+
+// 是否被 这个注释包括的 /* 包裹的中文
+function isWrapByStartComment (str, match, index, range = 500) {
+  const startIndex = index - 1
+  let end = startIndex - range
+  for (let i = startIndex; (i >= (end -1) || i >= 1); i--) {
+    // 如果先遇到*/ 则表示不是被包裹
+    if (str.charAt(i - 1) === '*' && str.charAt(i) === '/') {
+      return false
+    } else if (str.charAt(i - 1) === '/' && str.charAt(i) === '*') {
+      return true
+    }
+  }
+  return false
+}
+
+// 处理<script> 到 export default 中间的内容
+const scriptPreReg = new RegExp("script>([\\s\\S]+)(?:export\\s*default)", "i")
+
+function processVueFile (fileContent) {
+  let match = scriptPreReg.exec(fileContent)
+  if (match) {
+    let zhMatch;
+    while(zhMatch = i18nContentReg.exec(match[1])) {
+      // 忽略被/* */ 注释的中文
+      if (isWrapByStartComment(match[1], zhMatch[0], zhMatch.index)) {
+        continue;
+      }
+      // 忽略被// 注释的中文
+      if (isWrapByDoubelSlashComment(match[1], zhMatch[0], zhMatch.index)) {
+        continue;
+      }
+      console.log(zhMatch[0])
+    }
+  }
+}
+
+function run () {
+  vfs
+  .src(config.i18nFileRules.map(item => path.resolve(absoluteRootDir, item)),{
+      ignore: config.ignoreI18nFileRules.map(item => path.resolve(absoluteRootDir, item)),
+      dot: false
+    }
+  )
+  .pipe(
+    map((file, cb) => {
+      console.log('开始解析', file.path)
+      const extname = path.extname(file.path)
+      let fileContent = file.contents.toString()
+      let newFileContent
+      if (extname.toLowerCase() === '.vue') {
+        newFileContent = processVueFile(fileContent)
+      }
+      cb()
+    })
+  )
+  .on("end", () => {
+    console.log('全部处理完成')
+  });
+}
+
+run()
---
+++ test/example-check/src/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <p>测试文字</p>
+    <a 
+      :data="$t('您好')"
+      title="希望的田野" v-bind:href="cc" @click="handleClick" checked disabled
+      data-ad='aaa'
+      :target="cc">  希望的的田野   </a>
+  </div>
+</template>
+<script>
+/**
+ * 您好ccc
+ * 吗
+ * 过后打扫房间了
+ */
+
+// 您好233 fsafsaf你好3
+
+const message = "您好aa";
+
+/**
+ * 12312afsa你好22
+ */
+
+export default {
+  data () {
+    return {
+      hope: '希dd望',
+      i18n: this.$t ( '您好' ),
+      love: "你" // 内容
+    }
+  },
+  computed: {
+    world () {
+      return '世界'
+    }
+  }
+}
+</script>
